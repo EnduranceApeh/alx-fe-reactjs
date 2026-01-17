6e636060a3adf8f1ddac55eba4cdb3e69c27b2d8
EnduranceApeh <apehendurance2@gmail.com>
2026-01-17 10:16:46 +0100

    Added inline styling to the components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{backgroundColor: "black", color: "red", fontSize: "12px"}}>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  )
+}
+
+export default Footer
