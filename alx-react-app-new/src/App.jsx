c94c0807b2befc2854b41fcc18e5d451b0339c86
EnduranceApeh <apehendurance2@gmail.com>
2026-01-17 10:37:46 +0100

    Created counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import { UserProfile } from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 function App() {
@@ -17,9 +18,11 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
-      <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+      <Counter />
+      <Footer />
     </>
+      
   )
 }
 
