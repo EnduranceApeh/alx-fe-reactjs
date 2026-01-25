b45f02e3f2bb8b1d481f5e9a144f1ff5366a6f83
EnduranceApeh <apehendurance2@gmail.com>
2026-01-25 11:17:31 +0100

    Wrapped the omponenet with Router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,23 +3,26 @@ import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-    <Routes>
-      <Route
-       path="/recipe" 
-       element={
-        <>
-          <AddRecipeForm />
-          <RecipeList />
-        </>
-       } />
-      <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-    </Routes>
+
+    <Router>
+      <Routes>
+        <Route
+         path="/recipe"
+         element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+         } />
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
     </>
   )
 }
