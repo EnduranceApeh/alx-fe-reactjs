7dbf0da23bbdd204c96d3233ba3707be24d6a249
EnduranceApeh <apehendurance2@gmail.com>
2026-01-29 09:46:01 +0100

    Github user search set up files

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path="" element={}></Route>
+        <Route path="" element={}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
