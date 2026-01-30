e99a6f4b39e0bdd9bc95bf8a333e541940b3606e
EnduranceApeh <apehendurance2@gmail.com>
2026-01-30 13:51:16 +0100

    Added advanced user search

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 
-async function fetchUserData(username) {
-  try{
-    const response = await axios.get(`https://api.github.com/users/${username}`)
-    return response.data
+const githubApi = axios.create({
+  baseURL: "https://api.github.com",
+});
 
-  } catch(error) {
-    return error
-  }
-}
+const fetchUserData = async (username, location, minRepos) => {
+  let query = username || "";
+
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const response = await githubApi.get(`/search/users?q=${query}`);
+  return response.data; // contains items array
+};
 
 export default fetchUserData;
