54ff2d31a44ddd1f43d081f12e2ee7cf89ae751d
EnduranceApeh <apehendurance2@gmail.com>
2026-01-31 12:33:28 +0100

    Changed state variable name

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-async function fetchUserData(username, location, repoCount) {
+async function fetchUserData(username, location, minRepos) {
   try {
-    const query = `${username}+location:${location}+repos:>${repoCount}`;
+    const query = `${username}+location:${location}+repos:>${minRepos}`;
     const url = `https://api.github.com/search/users?q=${query}&per_page=30`;
     const response = await axios.get(url)
     console.log(response)
