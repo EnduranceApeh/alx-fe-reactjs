bb502479158cf7e4d7f420d508f46a4a6ac1a969
EnduranceApeh <apehendurance2@gmail.com>
2026-02-14 15:16:06 +0100

    Added shadw to recipe detail component

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -22,7 +22,7 @@ const RecipeDetail = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="max-w-4xl mx-auto p-6 shadow-lg">
       <img
         src={recipe.image}
         alt={recipe.title}
